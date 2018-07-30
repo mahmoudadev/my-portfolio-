36085e1a07ba76ef15119c4848532f60e161b23d
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-07-30 16:57:10 +0200

    added new and create actions to portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,26 @@ class PortfoliosController < ApplicationController
     @portfolios = Portfolio.all
   end
 
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      flash[:success] = "Portfolio added successfully"
+      redirect_to @portfolio
+    else
+      render 'new'
+    end
+
+  end
+
+
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title , :subtitle, :body)
+  end
+
 end
