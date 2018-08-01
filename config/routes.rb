c8588121ad42e257cb40566c44141d03c0055f61
Mahmoud <mahmoud.abdelmoneim.dev@gmail.com>
2018-08-01 17:12:41 +0200

    implemented basic authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios
   get 'pages/home'
   get 'pages/about'
@@ -8,5 +9,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
+
+  root to: "pages#home"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
